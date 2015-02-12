2bd4bb87b7c868cb0a74013222fa8c01438d9c0e
ayine Ernest <ayine17@gmail.com>
2015-02-12 01:12:42 +0000

    Create a mathematical statement in a single-line

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python's order of operations respects parentheses.
+
+Create a mathematical statement in a single-line
+"""
+
+WEEK=(19%10+100+2**8) /7
+
+
+print(WEEK)
