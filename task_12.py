6e64a324397db6ddaa116a39b12d6b2820880ff2
ayine Ernest <ayine17@gmail.com>
2015-02-12 07:13:24 +0000

    assign numeric types to varriable

---
+++ task_12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 12"""
+
+# import fractions module
+
+import fractions
+
+# import decimal module'
+
+import decimal
+
+INTVAL = 1
+
+FLOATVAL = 0.1
+
+DECVAL = decimal.Decimal('0.1')
+
+FRACVAL = fractions.Fraction(1, 10)
