6adf904d75b2179101d45e8fb056fa461196e378
ayine Ernest <ayine17@gmail.com>
2015-02-13 03:19:06 +0000

    task_09.py and task_11.py

--- task_09.py
+++ task_09.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+
+One way to achieve a multi-line string is to use triple double or single
+
+
+quotes.This is most commonly docstrings which are a required part of
+
+
+every module.
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
